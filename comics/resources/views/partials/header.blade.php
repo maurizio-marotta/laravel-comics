281332db41985759b4e22bcc33989afbed13a016
maurizio-marotta <mauriziomarotta@live.it>
2023-06-08 01:24:42 +0200

    inizio a importare l'header

---
+++ comics/resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="root">
+      <div class="logo">
+        <img src="/img/dc-logo.png" alt="logo">
+      </div>
+      <nav>
+        <ul>
+          <li>charaters</li>
+          <li>comics</li>
+          <li>movies</li>
+          <li>tv</li>
+          <li>games</li>
+          <li>collectibles</li>
+          <li>videos</li>
+          <li>fans</li>
+          <li>news</li>
+          <li>shop</li>
+        </ul>
+      </nav>
+    </div>
+</header>
