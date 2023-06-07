0a4d0408bfe69b5e10415fbf105d1a24b7741624
maurizio-marotta <mauriziomarotta@live.it>
2023-06-08 01:49:13 +0200

    importo il footer

--- comics/resources/views/partials/footer.blade.php
+++ comics/resources/views/partials/footer.blade.php
@@ -1,3 +1,153 @@
 <footer>
-    footer
+    <div class="root-3">
+        <div class="barra-link ">
+            <ul class="primo">
+                <li><img src="/img/buy-comics-digital-comics.png" alt="" width="42" height="42">
+                    Digital comics</li>
+                <li><img src="/img/buy-comics-merchandise.png" alt=""> Dc merch</li>
+                <li><img src="/img/buy-comics-subscriptions.png" alt=""> Subscription</li>
+                <li><img src="/img/buy-comics-shop-locator.png" alt=""> Comic shop location</li>
+                <li><img src="/img/buy-dc-power-visa.svg" alt="">Dc power visa</li>
+            </ul>
+        </div>
+        <div class="dc-logo">
+            <div class="barra-link-2 d-flex">
+                <ul class="seconda">
+                    <li><strong>Dc comics</strong></li>
+                    <li>charaters</li>
+                    <li>comics</li>
+                    <li>movies</li>
+                    <li>tv</li>
+                    <li>games</li>
+                    <li>collectibles</li>
+                </ul>
+                <ul class="seconda">
+                    <li><strong>Dc comics</strong></li>
+                    <li>charaters</li>
+                    <li>comics</li>
+                    <li>movies</li>
+                    <li>tv</li>
+                    <li>games</li>
+                    <li>collectibles</li>
+                </ul>
+                <ul class="seconda">
+                    <li><strong>Dc comics</strong></li>
+                    <li>charaters</li>
+                    <li>comics</li>
+                </ul>
+            </div>
+            <div class="barra-link-2 d-flex">
+                <ul class="seconda">
+                    <li><strong>Dc comics</strong></li>
+                    <li>charaters</li>
+                    <li>comics</li>
+                </ul>
+            </div>
+        </div>
+        <div class="sing-up d-flex">
+            <div class="log">sing-up now!</div>
+            <ul>
+                <li>Follow use</li>
+                <li><img src="/img/footer-facebook.png" alt=""></li>
+                <li><img src="/img/footer-periscope.png" alt=""></li>
+                <li><img src="/img/footer-pinterest.png" alt=""></li>
+                <li><img src="/img/footer-twitter.png" alt=""></li>
+                <li><img src="/img/footer-youtube.png" alt=""></li>
+            </ul>
+        </div>
+    </div>
 </footer>
+
+<style scoped>
+    .root-3 {
+        display: flex;
+        flex-direction: column;
+        background-image: url(/img/footer-bg.jpg);
+        height: 69vh;
+    }
+
+    .barra-link {
+        padding: 0px 150px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #0282F9;
+    }
+
+    img {
+        width: 40px;
+        height: 45px;
+    }
+
+    ul.primo {
+        margin: 10px;
+        display: flex;
+    }
+
+    ul.primo li {
+        color: white;
+        list-style: none;
+        margin: 10px;
+    }
+
+    ul.primo li:hover {
+        color: blue;
+        cursor: pointer;
+    }
+
+    ul.seconda {
+        color: white;
+    }
+
+    ul.seconda li:first-child {
+        margin: 10px 0px;
+        color: white;
+    }
+
+    ul.seconda li {
+        color: rgba(255, 255, 255, 0.473);
+        list-style: none;
+        font-size: 14px;
+    }
+
+    .barra-link-2 {
+        padding: 0 150px;
+    }
+
+    .dc-logo {
+        height: 100%;
+        background-image: url(../assets/img/dc-logo-bg.png);
+        background-repeat: no-repeat;
+        background-position: right;
+        margin-right: 50px;
+    }
+
+    .sing-up {
+        padding: 20px 150px;
+        background-color: #303030;
+        height: 80px;
+        justify-content: space-between;
+        align-items: baseline;
+    }
+
+    .log {
+        color: white;
+        border: 1px solid #0282F9;
+        padding: 10px;
+    }
+
+    .sing-up ul {
+        display: flex;
+        list-style: none;
+    }
+
+    .sing-up ul li img {
+        width: 20px;
+        height: 20px;
+        margin: 5px;
+    }
+
+    .sing-up ul li:first-child {
+        color: #11518D;
+    }
+</style>
--- comics/resources/views/partials/header.blade.php
+++ comics/resources/views/partials/header.blade.php
@@ -1,21 +1,59 @@
 <header>
     <div class="root">
-      <div class="logo">
-        <img src="/img/dc-logo.png" alt="logo">
-      </div>
-      <nav>
-        <ul>
-          <li>charaters</li>
-          <li>comics</li>
-          <li>movies</li>
-          <li>tv</li>
-          <li>games</li>
-          <li>collectibles</li>
-          <li>videos</li>
-          <li>fans</li>
-          <li>news</li>
-          <li>shop</li>
-        </ul>
-      </nav>
+        <div class="logo">
+            <img src="/img/dc-logo.png" alt="logo">
+        </div>
+        <nav>
+            <ul>
+                <li>charaters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </nav>
     </div>
 </header>
+
+<style scoped>
+    .root {
+        background-color: #FFFFFF;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0px 150px;
+        margin-top: 30px;
+    }
+
+    img {
+        width: 50px;
+    }
+
+    ul {
+        display: flex;
+    }
+
+    li {
+        list-style: none;
+        margin: 10px;
+    }
+
+    li:hover {
+        color: blue;
+        cursor: pointer;
+    }
+
+    li:hover::after {
+        content: '';
+        width: 100%;
+        display: block;
+        height: 4px;
+        background-color: blue;
+    }
+</style>
